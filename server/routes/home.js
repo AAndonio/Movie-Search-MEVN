b746224873bb395e52b5d3c83d56583c09615d97
Antonio Vivone <ndonio96@gmail.com>
2019-06-05 20:51:10 +0200

    Query generi funzionante

--- server/routes/home.js
+++ server/routes/home.js
@@ -26,48 +26,53 @@ router.post('/', async (req,res) => {
     var query = Movie.find({});
 
     if(req.body.color)
-    query.where('color').equals(req.body.color);
+        query.where('color').equals(req.body.color);
 
     if(req.body.min_year)
-    query.where('title_year').gt(req.body.min_year)
-
-    if(req.body.max_year)
-    query.where('title_year').lt(req.body.max_year);
+        query.where('title_year').gt(req.body.min_year).lt(req.body.max_year);
 
     if(req.body.min_duration)
-    query.where('duration').gt(req.body.min_duration).lt(req.body.max_duration);
+        query.where('duration').gt(req.body.min_duration).lt(req.body.max_duration);
 
     if(req.body.min_rating)
-    query.where('imdb_score').gt(req.body.min_rating).lt(req.body.max_rating);
+        query.where('imdb_score').gt(req.body.min_rating).lt(req.body.max_rating);
 
     if(req.body.content_rating)
-    query.where('content_rating').in(req.body.content_rating);
+        query.where('content_rating').in(req.body.content_rating);
 
-    if(req.body.genres){
-        req.body.genres.forEach((genre) => {
-            query.where('genres').in(el);
-        });
-    }
+    if(req.body.genres)
+        query.where('genres').all(req.body.genres);
     
-
     if(req.body.actors){
-        req.body.actors.forEach((el) => {
-            console.log(el);
-            query.or([{'actor_1_name': el}, {'actor_2_name': el}, {'actor_3_name': el}])
-        });
-    }
-
+        console.log(req.body.actors.length);
+        switch(req.body.actors.length){ 
+        case 1:
+            query.and(
+                {'actor_1_name': {$in: req.body.actors}},
+                {'actor_2_name': {$in: req.body.actors}},
+                {'actor_3_name': {$in: req.body.actors}}
+            );
+            break;
+        /*case 2:
+            query.where('actor_1_name').in(req.body.actors);
+            break;
+        case 3:
+            query.where('actor_1_name').in(req.body.actors);
+            break;*/
+        }
+    }  
+    
     if(req.body.director_name)
-    query.where('director_name').equals(req.body.director_name);
+        query.where('director_name').equals(req.body.director_name);
 
     if(req.body.min_budget)
-    query.where('budget').gt(req.body.min_budget).lt(req.body.max_budget);
+        query.where('budget').gt(req.body.min_budget).lt(req.body.max_budget);
 
     if(req.body.min_gross)
-    query.where('gross').gt(req.body.min_gross).lt(req.body.max_gross);
+        query.where('gross').gt(req.body.min_gross).lt(req.body.max_gross);
 
     if(req.body.min_critic)
-    query.where('num_critic_for_reviews').gt(req.body.min_critic).lt(req.body.max_critic);
+        query.where('num_critic_for_reviews').gt(req.body.min_critic).lt(req.body.max_critic);
     
     query.exec((err, movies) => {
         if(err){
